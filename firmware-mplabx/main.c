14e5804a4ecbb0763621d377fdeba6b2a17f60c0
Eduardo Camargo <cartola@gmail.com>
2014-04-15 00:57:37 -0300

    Inclusão de inversão de polaridade até a chegada do conversor.

--- firmware-mplabx/main.c
+++ firmware-mplabx/main.c
@@ -1,16 +1,33 @@
-#include <xc.h>
 #include <stdio.h>
+#include <xc.h>
 
-#define _XTAL_FREQ 32000000     // Velocidade do oscilador (interno ou externo)
+#define _XTAL_FREQ 16000000     // Velocidade do oscilador (interno ou externo)
 
-#pragma config FOSC = INTOSC    // Oscillator Selection bits (HS oscillator: High-speed crystal/resonator on RA6/OSC2/CLKOUT and RA7/OSC1/CLKIN)
+/*#pragma config FOSC = INTOSC    // Oscillator Selection bits (HS oscillator: High-speed crystal/resonator on RA6/OSC2/CLKOUT and RA7/OSC1/CLKIN)
 #pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
 #pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT enabled)
 #pragma config MCLRE = ON       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is digital input, MCLR internally tied to VDD)
 #pragma config BOREN = OFF      // Brown-out Detect Enable bit (BOD disabled)
 #pragma config LVP = OFF        // Low-Voltage Programming Enable bit (RB4/PGM pin has digital I/O function, HV on MCLR must be used for programming)
 #pragma config CPD = OFF        // Data EE Memory Code Protection bit (Data memory code protection off)
-#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+#pragma config CP = OFF*/         // Flash Program Memory Code Protection bit (Code protection off)
+#pragma config FOSC = INTOSC    // Oscillator Selection (INTOSC oscillator: I/O function on CLKIN pin)
+#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
+#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Memory Code Protection (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable (Brown-out Reset disabled)
+#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+#pragma config IESO = OFF       // Internal/External Switchover (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is disabled)
+
+// CONFIG2
+#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
+#pragma config PLLEN = ON       // PLL Enable (4x PLL enabled)
+#pragma config STVREN = OFF     // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will not cause a Reset)
+#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable (High-voltage on MCLR/VPP must be used for programming)
 
 ////////////////////////////////////////////////variaveis globais///////////////////////////////////////////////////////////
 char caracter;
@@ -36,18 +53,38 @@ void putch(char data) {
     TXREG = data;
 }
 
+void escreve(char *texto){
+    char c = texto[0];
+    int i = 0;
+    while (c != '\0') {
+        putch(c);
+        c = texto[++i];
+    }
+}
+
 /* Configura o clock do processador. */
 void configure_Oscillator() {
     /* Ajusta o clock para 32MHz. */
-    OSCCONbits.IRCF = 0b00001110; // Define o oscilador interno em 8MHz.
+   /* OSCCONbits.IRCF = 0b00001110; // Define o oscilador interno em 8MHz.
     OSCCONbits.SPLLEN = 0x01; // Liga o PPL 4x.
-    OSCCONbits.SCS = 0x00; // Seleciona a fonte como o oscilador interno.
+    OSCCONbits.SCS = 0x00; // Fonte do oscilador definido na configuração FOSC.
+   */
+    /* Ajusta o clock para 16MHz. */
+    OSCCONbits.IRCF = 0b00001111; // Define o oscilador interno em 16MHz.
+    OSCCONbits.SPLLEN = 0x00; // Desliga o PPL 4x.
+    OSCCONbits.SCS = 0x00; // Fonte do oscilador definido na configuração FOSC.
 }
 
 /* Configura as portas de entrada e saída. */
 void configure_Ports() {
     ANSELB = 0; // I/O digital em todas as portas B.
     TRISB = 0b00100000; // Configura RB5(12) (pino - RX) como entrada, o restante como saída. Nota, o pino RB7(10) é o TX.
+
+    /* Muda as portas RX e TX para RC5 e RC4 respectivamente. */
+    ANSELC = 0;
+    TRISC = 0b00100000;
+    APFCON0bits.RXDTSEL = 0b1; // Define porta RX em RC5(5).
+    APFCON0bits.TXCKSEL = 0b1; // Define porta TX em RC4(6).
 
     OPTION_REGbits.nWPUEN = 1; // Weak pullups habilitados individualmente.
 }
@@ -65,6 +102,9 @@ void configure_SerialPort(long baud_rate, int mode) {
     /* Desabilita as portas I2C e SPI */
     SSP1CON1bits.SSPEN = 0;
     SSP2CON1bits.SSPEN = 0;
+
+    /* Inverte polaridade */
+    BAUDCONbits.SCKP = 0b1;
     
     RCSTA = 0b10010000; // Habilita a porta serial, recepção de 8 bits em modo contínuo, assíncrono.
     int valor;          // Valor da configuração para o gerador de baud rate.
@@ -95,14 +135,13 @@ void main(void) {
     /* Configurações iniciais. */
     configure_Oscillator();
     configure_Ports();
-    configure_SerialPort(19200, 1); // Modo de alta velocidade
+    configure_SerialPort(9600, 1); // Modo de alta velocidade
     configure_Interrupts();
 
     printf("Usando a serial MPlab XC8 \n\r");
     printf("Digite algo \n\r");
     for (;;) {
         if (char_received) { // Possui dado a ser lido
-            printf(" \n\r caracter digitado :");
             putch(caracter);
             char_received = 0;
         }
